00b8f509638a603c5ff17288078021c917d66d56
Coralgoods <91501799+Coralgoods@users.noreply.github.com>
2022-03-07 21:37:06 -0500

    V2
    V2 test

--- AngularProject/AngularProject/DataObjects/Ticket.cs
+++ AngularProject/AngularProject/DataObjects/Ticket.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-// This method gets 
+// This is a test. Test two
 
 namespace AngularProject.DataObjects
 {
